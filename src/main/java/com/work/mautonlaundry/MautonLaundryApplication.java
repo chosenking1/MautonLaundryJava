c5550445c01d4f27e2167c3bec7b96d63917ad81
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2022-06-16 17:20:35 +0100

    first commit

---
+++ src/main/java/com/work/mautonlaundry/MautonLaundryApplication.java
@@ -0,0 +1,13 @@
+package com.work.mautonlaundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MautonLaundryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MautonLaundryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/mautonlaundry/MautonLaundryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.mautonlaundry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MautonLaundryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
