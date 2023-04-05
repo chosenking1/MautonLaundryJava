9cfd6cb719388e97de9f2d401a3b1646768d21ef
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2023-04-05 19:04:27 +0100

    Did Controller for Booking and Delivery model

--- src/main/java/com/work/mautonlaundry/controllers/BookingController.java
+++ src/main/java/com/work/mautonlaundry/controllers/BookingController.java
@@ -1,4 +1,57 @@
 package com.work.mautonlaundry.controllers;
 
+import com.work.mautonlaundry.data.model.Booking;
+import com.work.mautonlaundry.dtos.requests.bookingrequests.RegisterBookingRequest;
+import com.work.mautonlaundry.dtos.requests.bookingrequests.UpdateBookingRequest;
+import com.work.mautonlaundry.dtos.responses.bookingresponse.RegisterBookingResponse;
+import com.work.mautonlaundry.dtos.responses.bookingresponse.UpdateBookingResponse;
+import com.work.mautonlaundry.dtos.responses.bookingresponse.ViewBookingResponse;
+import com.work.mautonlaundry.services.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Collection;
+
+@RestController
 public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping("/registerBooking")
+    public RegisterBookingResponse registerBooking(@RequestBody RegisterBookingRequest request){
+        return bookingService.RegisterBooking(request);
+    }
+
+    @GetMapping("/viewBooking/{id}")
+    public ViewBookingResponse viewBooking(@PathVariable("id") Long id){
+
+        return bookingService.viewBooking(id);
+    }
+
+    @GetMapping("/viewAllBooking")
+    public Collection<Booking> viewAllBooking() {
+
+        return bookingService.getRepository().findAll();
+    }
+
+    @PutMapping("/updateBooking")
+    public UpdateBookingResponse updateBookingStatus(@RequestBody UpdateBookingRequest request)
+    {
+        return bookingService.bookingDetailsUpdate(request);
+    }
+
+    @DeleteMapping("/deleteBooking/{id}")
+    public ResponseEntity<?> deleteBooking(@PathVariable("id") Long id) {
+        try {
+            bookingService.deleteBooking(id);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/com/work/mautonlaundry/controllers/DeliveryController.java
+++ src/main/java/com/work/mautonlaundry/controllers/DeliveryController.java
@@ -1,4 +1,58 @@
 package com.work.mautonlaundry.controllers;
 
+import com.work.mautonlaundry.data.model.DeliveryManagement;
+import com.work.mautonlaundry.dtos.requests.deliverymanagementrequests.PickupRequest;
+import com.work.mautonlaundry.dtos.requests.deliverymanagementrequests.PickupStatusUpdateRequest;
+import com.work.mautonlaundry.dtos.responses.deliverymanagementresponse.PickupResponse;
+import com.work.mautonlaundry.dtos.responses.deliverymanagementresponse.PickupStatusResponse;
+import com.work.mautonlaundry.dtos.responses.deliverymanagementresponse.PickupStatusUpdateResponse;
+import com.work.mautonlaundry.services.DeliveryManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Collection;
+
+@RestController
 public class DeliveryController {
+
+    @Autowired
+    private DeliveryManagementService deliveryService;
+
+    @PostMapping("/registerPickup")
+    public PickupResponse registerPickup(@RequestBody PickupRequest request){
+        return deliveryService.createPickup(request);
+    }
+
+    @GetMapping("/viewDeliveryStatus/{id}")
+    public PickupStatusResponse viewDelivery(@PathVariable("id") Long id){
+
+        return deliveryService.findPickupById(id);
+    }
+
+    @GetMapping("/viewAllDelivery")
+    public Collection<DeliveryManagement> viewAllBooking() {
+
+        return deliveryService.getRepository().findAll();
+    }
+
+    @PutMapping("/updateDelivery")
+    public PickupStatusUpdateResponse updatePickupStatus(@RequestBody PickupStatusUpdateRequest request)
+    {
+        return deliveryService.pickupUpdate(request);
+    }
+
+    @DeleteMapping("/services/{id}")
+    public ResponseEntity<?> deleteService(@PathVariable("id") Long id) {
+        try {
+            deliveryService.deletePickup(id);
+            return ResponseEntity.ok().build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
